ff4b845ca0d76de8ef90a22b3954791c11870362
fonahia zagoto <45090409+fonahia@users.noreply.github.com>
2018-11-17 15:48:06 +0700

    Add files via upload

---
+++ KonversiSuhu/src/Celcius.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Celcius {
+        public double toCalvin(double n) {
+        double k;
+        k = n +273.15;
+        return k;
+    }public double toFahrenheit(double n) {
+        double k;
+        k = n *1.8 + 32;
+        return k;
+    }public double toCelcius (double n){
+        double k;
+        k = n;
+        return k;
+    }
+    
+}
